2ad24de1aa6c2cacdb02572e57d82d121e7a3259
Rodrigo Lopes <rodrigolmti@gmail.com>
2021-06-30 13:53:51 -0300

    Migrate code (#18)
    closes #17

--- lib/castle.dart
+++ lib/castle.dart
@@ -1,14 +1,13 @@
 
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 class Castle {
   static const MethodChannel _channel =
       const MethodChannel('castle');
 
-  static Future<void> configure({@required publishableKey, debugLoggingEnabled, maxQueueLimit, flushLimit, useCloudflareApp, baseURLAllowList}) async {
+  static Future<void> configure({required publishableKey, debugLoggingEnabled, maxQueueLimit, flushLimit, useCloudflareApp, baseURLAllowList}) async {
     await _channel.invokeMethod('configure', <String, dynamic>{
       'publishableKey': publishableKey,
       'debugLoggingEnabled': debugLoggingEnabled,
@@ -41,8 +40,8 @@ class Castle {
     await _channel.invokeMethod('flush');
   }
 
-  static Future<bool> flushIfNeeded(String url) async {
-    final bool flushIfNeeded = await _channel.invokeMethod('flushIfNeeded', <String, dynamic>{
+  static Future<bool?> flushIfNeeded(String url) async {
+    final bool? flushIfNeeded = await _channel.invokeMethod('flushIfNeeded', <String, dynamic>{
       'url': url,
     });
     return flushIfNeeded;
@@ -52,48 +51,48 @@ class Castle {
     await _channel.invokeMethod('reset');
   }
 
-  static Future<String> get baseUrl async {
-    final String baseUrl = await _channel.invokeMethod('baseUrl');
+  static Future<String?> get baseUrl async {
+    final String? baseUrl = await _channel.invokeMethod('baseUrl');
     return baseUrl;
   }
 
-  static Future<String> get clientId async {
-    final String clientId = await _channel.invokeMethod('clientId');
+  static Future<String?> get clientId async {
+    final String? clientId = await _channel.invokeMethod('clientId');
     return clientId;
   }
 
-  static Future<String> get clientIdHeaderName async {
-    final String clientIdHeaderName = await _channel.invokeMethod('clientIdHeaderName');
+  static Future<String?> get clientIdHeaderName async {
+    final String? clientIdHeaderName = await _channel.invokeMethod('clientIdHeaderName');
     return clientIdHeaderName;
   }
 
-  static Future<String> get createRequestToken async {
-    final String token = await _channel.invokeMethod('createRequestToken');
+  static Future<String?> get createRequestToken async {
+    final String? token = await _channel.invokeMethod('createRequestToken');
     return token;
   }
 
-  static Future<String> get requestTokenHeaderName async {
-    final String requestTokenHeaderName = await _channel.invokeMethod('requestTokenHeaderName');
+  static Future<String?> get requestTokenHeaderName async {
+    final String? requestTokenHeaderName = await _channel.invokeMethod('requestTokenHeaderName');
     return requestTokenHeaderName;
   }
 
-  static Future<String> get userId async {
-    final String userId = await _channel.invokeMethod('userId');
+  static Future<String?> get userId async {
+    final String? userId = await _channel.invokeMethod('userId');
     return userId;
   }
 
-  static Future<String> get userSignature async {
-    final String userSignature = await _channel.invokeMethod('userSignature');
+  static Future<String?> get userSignature async {
+    final String? userSignature = await _channel.invokeMethod('userSignature');
     return userSignature;
   }
 
-  static Future<String> get userAgent async {
-    final String userAgent = await _channel.invokeMethod('userAgent');
+  static Future<String?> get userAgent async {
+    final String? userAgent = await _channel.invokeMethod('userAgent');
     return userAgent;
   }
 
-  static Future<int> get queueSize async {
-    final int queueSize = await _channel.invokeMethod('queueSize');
+  static Future<int?> get queueSize async {
+    final int? queueSize = await _channel.invokeMethod('queueSize');
     return queueSize;
   }
 }
